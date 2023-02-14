987bc130eed822a4e132245123cae78b7c1c81f6
Denis Feliciano <denisf.salles@gmail.com>
2023-02-13 22:47:08 -0300

    chore: add mock list

--- src/app/polo.service.ts
+++ src/app/polo.service.ts
@@ -57,32 +57,14 @@ export class PoloService {
   private getAllPolosFromUserLocalization(): Polo[] {
 
     // TODO: request for api all polos based on user location
-
-    return [
-      {
-        id: 1,
-        name: 'Univesp Vila Nova Curuça',
-        distance: '7km',
-        address: 'Av. Getúlio Vargas, nº 1200 - Vila Nova Santana, Assis. CEP: 19807-130.	',
-        city: 'São Paulo',
-        state: 'SP',
-        location: {
-          latitude: -123,
-          longitude: 213,
-        }
+    return this.polos.map(polo => ({
+      distance: '7km',
+      location: {
+        latitude: -123,
+        longitude: 213,
       },
-      {
-        id: 2,
-        name: 'Univesp Carrão',
-        distance: '7km',
-        address: 'Av. Getúlio Vargas, nº 1200 - Vila Nova Santana, Assis. CEP: 19807-130.	',
-        city: 'São Paulo',
-        state: 'SP',
-        location: {
-          latitude: -123,
-          longitude: 213,
-        }
-      }
-    ];
+      ...polo,
+    }));
+
   }
 }
